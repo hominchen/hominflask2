c283b887fd792efabf7e86285773caa29e7d7a0c
unknown <chenhomin2020@gmail.com>
2022-08-24 13:46:11 +0800

    post edit page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from enum import unique
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
@@ -179,7 +179,7 @@ def get_current_date():
     return favorite_pizza
     # return {'Date': date.today()}
 
-#10 Post
+#10-1 Post
 @app.route('/add-post', methods=['GET', 'POST'])
 def add_post():
     form=PostForm()
@@ -198,12 +198,36 @@ def add_post():
         db.session.commit()
         flash("Blog 訊息張貼成功")
     return render_template("add_post.html", form=form)
-
-#11 Posts文章集
+#10-2 Posts文章集
 @app.route('/posts')
 def posts():
     posts = Posts.query.order_by(Posts.date_posted)
     return render_template('posts.html', posts=posts)
+#10-3 post內文
+@app.route('/posts/<int:id>')
+def post(id):
+    post=Posts.query.get_or_404(id)
+    return render_template('post.html', post=post)
+#10-4 edit文章編輯
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit_post(id):
+    post = Posts.query.get_or_404(id)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.author = form.author.data
+        post.slug = form.slug.data
+        post.content = form.content.data
+        db.session.add(post)
+        db.session.commit()
+        flash('文章已更新成功！')
+        return redirect(url_for('post', id=post.id))
+    # 先前資料
+    form.title.data = post.title
+    form.author.data = post.author
+    form.slug.data = post.slug
+    form.content.data = post.content
+    return render_template('edit_post.html', form=form)
 
 #03 錯誤頁面
 @app.errorhandler(404)
