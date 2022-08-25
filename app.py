671a4668d7729d2c0dced23d0405ef4075f42fcf
unknown <chenhomin2020@gmail.com>
2022-08-25 14:13:28 +0800

    clean db

--- app.py
+++ app.py
@@ -328,6 +328,8 @@ class Users(db.Model, UserMixin):
     favorite_color = db.Column(db.String(120))
     password_hash = db.Column(db.String(128))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # relationship
+    posts = db.relationship('Posts', backref="poster")
 
     # Hash password
     @property
@@ -347,9 +349,11 @@ class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
     content = db.Column(db.Text)
-    author = db.Column(db.String(255))
+    # author = db.Column(db.String(255))
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
     slug = db.Column(db.String(255))
+    # ForeignKey
+    poster_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
 if __name__ == "__main__":
